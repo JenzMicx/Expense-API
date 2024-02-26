7ce689a655fb5e135016363c423adb1993e57266
JenzMicx <Jenarong.ikm@gmail.com>
2024-02-26 10:35:44 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 using System.Net;
+using System.Text;
 using Auth_API.DbContext;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,9 +20,9 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 });
 
 //Add Identity
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-.AddEntityFrameworkStores<ApplicationDbContext>()
-.AddDefaultTokenProviders();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>() //ใช้ในการ DB ผู้ใช้และบทบาทใน Identity Framework
+.AddEntityFrameworkStores<ApplicationDbContext>() //ใช้ในการจัดการ DB ของ Identity Framework
+.AddDefaultTokenProviders(); //ใช้ในการจัดการ token 
 
 //Config Idenfity
 builder.Services.Configure<IdentityOptions>(options =>
@@ -39,9 +41,22 @@ builder.Services.AddAuthentication(options =>
     options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
+})
+.AddJwtBearer(options =>
 {
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true, //ตรวจสอบ Issuer ของ token ว่าถูกต้องไหม
+        ValidateAudience = true, //ตรวจสอบ Audience ของ token ว่าถูกต้องไหม
+        // ValidateLifetime = false, //ตรวจสอบว่า token มีอายุหมดอายุหรือยัง
+        // ValidateIssuerSigningKey = true, //ตรวจสอบความถูกต้องของคีย์สำหรับเซ็นต์ token
+        ValidIssuer = builder.Configuration["JWT:ValidIssuer"], //เป็น URL ของระบบหรือเว็บไซต์ที่ออก Token
+        ValidAudience = builder.Configuration["JWT:ValidAudience"], //เป็น URL ของบริการที่จะรับ Token เพื่อใช้งาน
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
 
+    };
 });
 
 
@@ -57,6 +72,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
