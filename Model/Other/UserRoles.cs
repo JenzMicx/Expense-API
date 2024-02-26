1c779ead3972fc3b0a3b6bebbbe23e4d388b2270
JenzMicx <Jenarong.ikm@gmail.com>
2024-02-26 11:03:40 +0700

    Create UserRoles.cs

---
+++ Model/Other/UserRoles.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth_API.Model.Other
+{
+    public static class UserRoles
+    {
+        public const string OWNER = "OWNER";
+        public const string ADMIN = "ADMIN";
+        public const string USER = "USER";
+    }
+}
