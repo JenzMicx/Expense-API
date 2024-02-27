c2e37cb624fc51782e673d8c0d824818a1f1c146
JenzMicx <Jenarong.ikm@gmail.com>
2024-02-27 15:17:19 +0700

    Create AddUserFieldModel.cs

---
+++ Model/Entities/AddUserFieldModel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Auth_API.Model.Entities
+{
+    public class AddUserFieldModel: IdentityUser
+    {
+        //เพิ่ม field ใน database ของ IdentityUser ที่มาจาก .net โดยสร้าง class ที่สืบทอดมาจาก IdentityUser อีกที
+        public string FristName {get; set;}
+        public string LastName {get; set;}
+        public int PhoneNumber {get; set;}
+    }
+}
