d2e0949f778b0ac92f8ca0629291af38f04c3109
JenzMicx <Jenarong.ikm@gmail.com>
2024-03-05 13:54:59 +0700

    Update AuthController.cs

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,20 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 using Auth_API.Model;
-using Auth_API.Model.Entities;
-using Auth_API.Model.Other;
 using Auth_API.Services.Interfaces;
-using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
 
 namespace Auth_API.Controllers
 {
