2de09823ca68999b68fe17837df48459734dcff9
JenzMicx <Jenarong.ikm@gmail.com>
2024-03-06 23:30:08 +0700

    Update ExpenseModel.cs

--- Model/Expenses/ExpenseModel.cs
+++ Model/Expenses/ExpenseModel.cs
@@ -1,25 +1,16 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Auth_API.Model.Expenses
 {
     public class ExpenseModel
     {
         [Key]
-        public int id { get; set; }
-        
-        [Required(ErrorMessage = "Title is required")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
         public string Title { get; set; }
-
-        [Required(ErrorMessage = "Amount is required")]
+        public string DateTime { get; set; }
         public double Amount { get; set; }
-
-        [Required(ErrorMessage = "Date is required")]
-        public DateTime SpendingDate { get; set; } = DateTime.Now;
-
         public string Category { get; set; }
     }
 }
